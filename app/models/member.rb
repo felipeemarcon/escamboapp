893cca5dd1cec84b1767cb55c9134299c50f40ce
Felipe Marcon <felipeemarcon@gmail.com>
2018-12-05 13:10:56 -0200

    Validating nested_attrbiutes member

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,10 +7,22 @@ class Member < ActiveRecord::Base
   # RatyRate Gem 
   ratyrate_rater
 
+  # Validations
+  validate :nested_attributes
+
   # Associations
   has_many :ads
   has_many :comments
   has_one :profile_member
   accepts_nested_attributes_for :profile_member
   
+  def nested_attributes
+    if nested_attributes_blank?
+      errors.add(:base, "É necessário preencher os campos Nome e Sobrenome.")
+    end
+  end
+
+  def nested_attributes_blank?
+    profile_member.first_name.blank? || profile_member.last_name.blank?    
+  end
 end
